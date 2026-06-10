517e5581a219a0cc853e6e717e8df93eed139b5a
robelahamed7374-maker <robelahamed7374@gmail.com>
2026-06-10 20:48:07 +0600

    Refactor openBox function and improve data handling

--- script.js
+++ script.js
@@ -1,37 +1,206 @@
-function openBox(name){
+const form = document.getElementById("setupForm");
+const list = document.getElementById("savedList");
+const resetBtn = document.getElementById("resetBtn");
 
-if(name==="Reports খুলবে"){
+loadData();
 
-location.href=
-"reports.html";
+form.addEventListener("submit", function(e){
+e.preventDefault();
+
+const file =
+document.getElementById("photo").files[0];
+
+const reader = new FileReader();
+
+reader.onload = function(){
+
+const data = {
+name:
+document.getElementById("name").value,
+
+market:
+document.getElementById("market").value,
+
+pair:
+document.getElementById("pair").value,
+
+win:
+document.getElementById("win").value,
+
+notes:
+document.getElementById("notes").value,
+
+video:
+document.getElementById("video").value,
+
+desc:
+document.getElementById("desc").value,
+
+img:
+reader.result || "",
+
+date:
+new Date().toLocaleString()
+};
+
+let setups =
+JSON.parse(
+localStorage.getItem("rbTrading")
+)||[];
+
+setups.unshift(data);
+
+localStorage.setItem(
+"rbTrading",
+JSON.stringify(setups)
+);
+
+form.reset();
+
+loadData();
+
+};
+
+if(file){
+
+reader.readAsDataURL(file);
+
+}else{
+
+reader.onload();
 
 }
 
-if(name==="Media খুলবে"){
+});
 
-location.href=
-"media.html";
+function loadData(){
+
+list.innerHTML="";
+
+let setups=
+JSON.parse(
+localStorage.getItem("rbTrading")
+)||[];
+
+document.getElementById(
+"total"
+).innerText=
+"Total: "+setups.length;
+
+setups.forEach((x,i)=>{
+
+list.innerHTML+=`
+
+<div class="saveCard">
+
+<div>
+
+${
+x.img
+?
+
+`<img src="${x.img}"
+class="preview">`
+
+:
+
+`<div class="preview"></div>`
 
 }
 
-if(name==="Notes খুলবে"){
+</div>
 
-location.href=
-"notes.html";
+<div class="info">
+
+<h3>${x.name}</h3>
+
+<p>Market:
+${x.market}</p>
+
+<p>Pair:
+${x.pair}</p>
+
+<p>Win Rate:
+${x.win}</p>
+
+<p>${x.notes}</p>
+
+<p>${x.video}</p>
+
+<p>${x.desc}</p>
+
+<small>${x.date}</small>
+
+</div>
+
+<div class="actions">
+
+<button
+onclick="saveAgain(${i})"
+class="green">
+
+Save
+
+</button>
+
+<button
+onclick="removeData(${i})"
+class="red">
+
+Delete
+
+</button>
+
+</div>
+
+</div>
+
+`;
+
+});
 
 }
 
-if(name==="History খুলবে"){
+function removeData(i){
 
-location.href=
-"history.html";
+let setups=
+JSON.parse(
+localStorage.getItem(
+"rbTrading"
+));
+
+setups.splice(i,1);
+
+localStorage.setItem(
+"rbTrading",
+JSON.stringify(setups)
+);
+
+loadData();
 
 }
 
-if(name==="Settings খুলবে"){
+function saveAgain(i){
 
-location.href=
-"settings.html";
+alert(
+"আগেই সেভ করা আছে"
+);
+
+}
+
+resetBtn.onclick=()=>{
+
+if(
+confirm(
+"সব ডিলিট করবেন?"
+)
+){
+
+localStorage.removeItem(
+"rbTrading"
+);
+
+loadData();
 
 }
 
